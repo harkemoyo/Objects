da32d33c143796a1c4213ac0f2abbf7dcc81f481
harkemoyo <markndeche91@gmail.com>
2023-06-12 15:36:57 +0300

    added commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert('hello world');
+// alert('hello world');
 const magic = "mark";
 const age = 28;
 
@@ -13,3 +13,31 @@ const sum = {
 }
 sum.sumAge()
 
+// factory fnc
+
+function coolPhoto(name, age) {
+ return{
+name,
+age,
+color() {
+    console.log( `This is ${this.name} favourite color`);
+}
+};
+};
+const obj1 = coolPhoto('Sifa', 29);
+console.log(obj1);
+
+// Create a constructor function
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+  
+  // Create two Person objects
+  let john = new Person('john', 25);
+  let jane = new Person('jane', 30);
+  
+  console.log(john);
+  console.log(jane);
